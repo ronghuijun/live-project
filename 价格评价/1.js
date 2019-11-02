0da8a9e8899eb2a80512a47a8e5af7da58a16728
jihuayu <8042833@qq.com>
2019-11-02 09:03:44 +0800

    up

---
+++ 价格评价/1.js
@@ -0,0 +1,21 @@
+const fs = require("fs")
+const csv=require('csvtojson')
+let ans = []
+csv()
+.fromFile("1.csv")
+.then((json)=>{
+    for(let i in json){
+        let a = json[i]
+        if(a.avgprice>200){
+            ans.push({title:a.title,mark:count(a.avgscore,a.comments,a.avgprice)});
+        }
+    }
+    ans.sort((a,b)=>{
+        return b.mark - a.mark;
+    })
+    fs.writeFileSync("200.json",JSON.stringify(ans));
+})
+
+function count(mark,comment,price){
+    return (comment/4000+mark)/price
+}
